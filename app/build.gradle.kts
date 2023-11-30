562710e92d099e2c44519c3572524457cb928175
Xiaojing Wang <wang0936@algonquinlive.com>
2023-11-29 20:59:41 -0500

    step1: volly sucessed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,12 @@ plugins {
 
 android {
     namespace = "algonquin.cst2335.cst2335_finalproject"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 
     defaultConfig {
         applicationId = "algonquin.cst2335.cst2335_finalproject"
@@ -15,6 +20,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -33,9 +39,15 @@ android {
 
 dependencies {
 
+    var room_version = "2.4.2"
+    implementation ("androidx.room:room-runtime:$room_version")
+    annotationProcessor ("androidx.room:room-compiler:$room_version")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
@@ -1,14 +1,70 @@
 package algonquin.cst2335.cst2335_finalproject;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import algonquin.cst2335.cst2335_finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    RequestQueue queue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.lookupButton.setOnClickListener(click -> {
+
+            String latitude = binding.editTextLatitude.getText().toString();
+            String longitude = binding.editTextLongitude.getText().toString();
+
+            String url = "https://api.sunrisesunset.io/json?lat="
+                    + latitude + "&lng="
+                    + longitude + "&timezone=CA&date=today";
+
+            queue = Volley.newRequestQueue(this);
+
+            // 发送网络请求
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    response ->{
+                        try{
+                                JSONObject obj = response.getJSONObject("results");
+                                String sunrise = obj.getString("sunrise");
+                                String sunset = obj.getString("sunset");
+
+                                runOnUiThread(()->{
+                                    binding.sunrise.setText("The current sunrise time is " + sunrise);
+                                    binding.sunrise.setVisibility(View.VISIBLE);
+
+                                    binding.sunset.setText("The current sunset time is " + sunset);
+                                    binding.sunset.setVisibility(View.VISIBLE);
+                                });
+
+                        } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                        }
+                    },
+                    error -> {
+                        int i = 0;
+                    });
+                    queue.add(request);
+            });
     }
 }
+
+
+
