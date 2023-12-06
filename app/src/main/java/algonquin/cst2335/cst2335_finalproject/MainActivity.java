15ae938b4c7aa5174782d8850b499f8ff8966bf6
long <liu00590@algonquinlive.com>
2023-12-06 08:54:10 -0500

    Volly commit error

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
@@ -2,8 +2,14 @@ package algonquin.cst2335.cst2335_finalproject;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -14,25 +20,31 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import algonquin.cst2335.cst2335_finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected String recipe;
+
+
     protected RequestQueue queue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         queue = Volley.newRequestQueue(this);
 
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+
         binding.searchButton.setOnClickListener( click -> {
-            String recipeTitle = URLEncoder.encode(binding.passwordText.getText().toString());
+            String recipeTitle = URLEncoder.encode(binding.recipeTitleText.getText().toString());
 
             String url = "https://api.spoonacular.com/recipes/complexSearch?query="+ recipeTitle +"&apiKey=9af43493ab344fc8bbaee3b9aef17483";
 
@@ -46,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
                       int id = recipe.getInt("id");
                       String title = recipe.getString("title");
                       String image = recipe.getString("image");
-
 
                       String recipeURL = "https://api.spoonacular.com/recipes/"+ id +"/information?apiKey=9af43493ab344fc8bbaee3b9aef17483";
 
@@ -76,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         }
+
 }
 
 
